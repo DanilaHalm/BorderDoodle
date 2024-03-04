8256bc0e0a7beba5de76d66e3927ed9d284d1f50
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2024-03-04 21:24:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let boardWidth = 350;
 let boardHeight = 700;
 let context;
 
-window.onload = () => {
+window.onload = function() {
   board = document.getElementById("board");
   board.width = boardWidth;
   board.height = boardHeight;
