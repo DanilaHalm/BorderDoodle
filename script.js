5b111b50cf1864f2a730d3bfff2974b1cee3fea0
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2024-03-04 21:53:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let board;
-let boardWidth = 370;
-let boardHeight = 600;
+let board = document.getElementById("board");
+let boardWidth = board.style.width;
+let boardHeight = board.style.height;
 let context;
 
 window.onload = function() {
-  board = document.getElementById("board");
-  board.width = boardWidth;
-  board.height = boardHeight;
+//  board = document.getElementById("board");
+//  board.width = boardWidth;
+//  board.height = boardHeight;
   context = board.getContext("2d");
 }
 
