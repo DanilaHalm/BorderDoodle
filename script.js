3d2094618c13c53f07f87629981b74a4b2622333
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2024-03-04 22:32:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,29 @@ let boardWidth = !(board.style.width % 2) ? board.style.width : board.style.widt
 let boardHeight = board.style.height;
 let context;
 
+//doodler
+let doodlerWidth = 50;
+let doodlerHeight = 50;
+let doodlerX = boardWidth/2 - doodlerWidth/2;
+let doodlerY = boardHeight - doodlerHeight*2;
+
+let doodler = {
+  width: doodlerWidth,
+  height: doodlerHeight,
+  x: doodlerX,
+  y: doodlerY,
+}
+
 window.onload = function() {
-//  board = document.getElementById("board");
-//  board.width = boardWidth;
-//  board.height = boardHeight;
   context = board.getContext("2d");
+
+  context.fillStyle("red");
+  context.fillRect(
+    doodler.x,
+    doodler.y,
+    doodler.width,
+    doodler.height
+  );
+  
 }
 
